a25c5c7052b07bb5fbe7fe42b7fa4543a4b583bd
“anstarky” <anstarky@gmail.com>
2020-01-31 00:02:27 +0200

    Updates

---
+++ precache-manifest.ea0a0db5df65f6b71fb75fdb7f22a96d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: '86ab930d7b7170af692e9776a868479b',
+    url: '/goit-react-hw-05-budget-planner/index.html',
+  },
+  {
+    revision: 'c5ec7652c408cdf7b1f4',
+    url: '/goit-react-hw-05-budget-planner/static/css/main.33bd9b1c.chunk.css',
+  },
+  {
+    revision: '748f095f6ccdb2e8fff9',
+    url: '/goit-react-hw-05-budget-planner/static/js/2.6ca95193.chunk.js',
+  },
+  {
+    revision: 'c5ec7652c408cdf7b1f4',
+    url: '/goit-react-hw-05-budget-planner/static/js/main.5d1007d9.chunk.js',
+  },
+  {
+    revision: 'dffe8cd1e2fc699743fb',
+    url: '/goit-react-hw-05-budget-planner/static/js/runtime-main.d2e8316a.js',
+  },
+]);
